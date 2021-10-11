c7e0f281a7ab1de01c6db81479385c198f6db57a
@ paquito05 <you@example.com>
2021-10-11 12:12:09 -0500

    canvas-kiosko

---
+++ database/config.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN, {
+           //useNewUrlParser: true,
+            useUnifiedTopology: true,
+            //useCreateIndex: false,
+            //useFindAndModify: false 
+        });
+
+        console.log('base de datos en Mongo Atlas online');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la base de datos.');
+    }
+}
+
+module.exports = {
+    dbConnection
+}
